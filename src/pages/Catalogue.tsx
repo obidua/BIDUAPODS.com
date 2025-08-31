98a5a016d1b65f1c41434c4ba1aa05639de3e0e3
obidua <64002560+obidua@users.noreply.github.com>
2025-09-01 03:24:08 +0530

    Updated Catalogue.tsx

--- src/pages/Catalogue.tsx
+++ src/pages/Catalogue.tsx
@@ -1,4 +1,4 @@
-      {showMainContent && (
+{showMainContent && (
         <section className="py-20 bg-gray-50/70 dark:bg-gray-900/70">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <motion.div 
@@ -18,3 +18,7 @@
           </div>
         </section>
       )}
+  );
+};
+
+export default Catalogue;
