825d819e12fde07e327807e848a5be37da6677b3
obidua <64002560+obidua@users.noreply.github.com>
2025-08-31 23:10:44 +0530

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -213,7 +213,8 @@ const Home: React.FC = () => {
                 key={series.id}
                 initial={{ opacity: 0, y: 40, scale: 0.9 }}
                 whileInView={{ opacity: 1, y: 0, scale: 1 }}
-                transition={{ duration: 0.8, delay: index * 0.2 }}
+                transition={{ duration: 0.5, delay: index * 0.1 }}
+                viewport={{ once: true, margin: "-100px" }}
               >
                 <SeriesCard series={series} />
               </motion.div>
